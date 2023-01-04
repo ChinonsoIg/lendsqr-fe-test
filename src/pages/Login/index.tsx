3c901ed8325a15088c57629bd2a908acd3bfe449
ChinonsoIg <ojononso@yahoo.com>
2023-01-04 22:49:18 +0100

    design desktop site for login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+import "../../assets/styles/Login.scss";
+import "../../assets/styles/index.scss";
+import logo_login from "../../assets/images/logo_login.png";
+import pablo_sign_in from "../../assets/images/pablo_sign_in.png";
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <div className="login-image-box">
+        <img src={logo_login} alt="logo" className="logo_login" />
+        <img src={pablo_sign_in} alt="logo" className="pablo_sign_in" />
+      </div>
+      <div className="login-form-box">
+        <div className="title_box">
+        <header className="login_title">Welcome!</header>
+        <p className="login_subtitle">Enter details to login</p>
+        </div>
+        <form className="login_form">
+          <div className="inputs_box">
+            <input />
+            <input />
+            <p>Forgot password?</p>
+          </div>
+          <input type="submit" value="Log in" className="login_btn" />
+        </form>
+      </div>
+    </div>
   )
 }
 
