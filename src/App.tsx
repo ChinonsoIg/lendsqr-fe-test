17614152e36945694867b6ccbb1c9d10b51bb2cc
ChinonsoIg <ojononso@yahoo.com>
2023-01-07 06:57:17 +0100

    implement router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { matchRoutes, useLocation } from "react-router-dom"
 import React from 'react';
 // import logo from './logo.svg';
 // import './assets/styles/App.scss';
 import './assets/styles/index.scss';
 import Login from './pages/Login';
 import SharedLayout from './components/layout/SharedLayout';
-import Dashboard from './pages/Dashboard';
+import Users from './pages/Users';
 
 function App() {
+
   return (
-    <>
-      {/* <Login /> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login />}>
-            {/* <Route index element={<div>Hello world!</div>} /> */}
-
-          </Route>
-          <Route path='users' element={<div>Hello world users!</div>}>
-            {/* <Route index element={<Dashboard />} /> */}
-            <Route path='users/:userId' element={<div>Hello world! user id</div>} />
-          </Route>
-
-          <Route path='products' element={<div>Hello world products!</div>}>
-            <Route index element={<div>HProduct!</div>} />
-            <Route path=':productId' element={<div>Hello world single product!</div>} />
-          </Route>
-          <Route path='*' element={<div>Error!</div>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Login />} />
+        </Route>
+        <Route path='/users'>
+          <Route index element={<Users />} />
+          <Route path=':userId' element={<div>userId</div>} />
+        </Route>
+        <Route path='*' element={<div>Error!</div>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Link,
-} from "react-router-dom";
 
 import './assets/styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
-    ),
-  },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <RouterProvider router={router} /> */}
+      <App />
   </React.StrictMode>
 );
 
